b6096f855f7c7760759d8d5a2812d9b324c3dacf
AdrianT99 <adriantenorior99@gmail.com>
2021-08-02 00:37:44 -0600

    Random animals fact project

---
+++ MixedMessages.js
@@ -0,0 +1,11 @@
+let animalFacts = ["Adult lions pretend to be hurt by the bites of their young to encourage their strength."
+,"Rats are not physically capable of vomiting, hence why Rat Poison is so effective.",
+"Sloths sometimes mistake their own arms for branches, causing them to fall to their death.",
+"Cows ‘moo’ in accents specific to their region. They have dialects, just like humans.",
+"Male puppies will sometimes let female puppies ‘win’ during play fights in order to get to know them better.",
+"Scorpions can hold their breath for up to 6 days.",
+"A shrimp’s heart is in his head","The longest recorded flight by a chicken was just over 13 seconds.",
+"Cows have best friends and can become stressed when separated from them."];
+
+let randomFact = Math.floor(Math.random()*animalFacts.length);
+console.log(animalFacts[randomFact]);
